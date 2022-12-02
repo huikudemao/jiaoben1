d882bf7a5159a20965142aab7c58fac9d535f547
huicudemao <1365560542@qq.com>
2022-12-03 03:12:32 +0800

    第四次优化

--- temp.js
+++ temp.js
@@ -1,10 +1,46 @@
 // ==UserScript==
 // @name         hello lzp
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world! lianxi!
 // @author       You
 // @match        *://*
+// @match        *://pan.baidu.com/disk/home*
+// @match        *://yun.baidu.com/disk/home*
+// @match        *://pan.baidu.com/disk/main*
+// @match        *://yun.baidu.com/disk/main*
+// @match        *://pan.baidu.com/s*
+// @match        *://yun.baidu.com/s*
+// @match        *://*.youku.com/*
+// @match        *://*.iqiyi.com/*
+// @match        *://*.iq.com/*
+// @match        *://*.le.com/*
+// @match        *://v.qq.com/*
+// @match        *://m.v.qq.com/*
+// @match        *://*.tudou.com/*
+// @match        *://*.mgtv.com/*
+// @match        *://tv.sohu.com/*
+// @match        *://film.sohu.com/*
+// @match        *://*.1905.com/*
+// @match        *://*.bilibili.com/*
+// @match        *://*.pptv.com/*
+// @match        *://item.taobao.com/*
+// @match        *://chaoshi.detail.tmall.com/*
+// @match        *://detail.tmall.com/*
+// @match        *://detail.tmall.hk/*
+// @match        *://item.jd.com/*
+// @match        *://*.yiyaojd.com/*
+// @match        *://*.liangxinyao.com/*
+// @match        *://music.163.com/*
+// @match        *://y.qq.com/*
+// @match        *://*.kugou.com/*
+// @match        *://*.kuwo.cn/*
+// @match        *://*.ximalaya.com/*
+// @match        *://*.zhihu.com/*
+// @match        *://*.douyin.com/*
+// @match        *://*.kuaishou.com/*
+// @match        *://*.ixigua.com/*
+// @match        *://*.youtube.com/*
 // @icon         none
 // @license      AGPL License
 // @grant        none
