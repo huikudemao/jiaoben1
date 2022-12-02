67036efde2d131d50fdf75eb746ccc13cc28bcb3
huicudemao <1365560542@qq.com>
2022-12-03 02:50:41 +0800

    第三次

--- temp.js
+++ temp.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hello lzp
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world! lianxi!
 // @author       You
 // @match        *://*
@@ -14,6 +14,28 @@
 (function() {
     // 启用严格模式
     'use strict';
+    var img2=document.createElement('img');
+    document.body.appendChild(img2);
+    img2.src="https://pic.baike.soso.com/p/20120724/20120724100154-699505001.jpg";
+    img2.style.cssText="width:75px;height:75px;position: absolute;"
 
-    console.log("第一次写脚本，第二次提交")
+    // 鼠标移动事件
+    document.onmousemove =function(ev) {
+        Ev = ev || window.event;
+        var mousePos = mouseCoords(ev);
+
+        img2.style.left=mousePos.x+2+'px';
+        img2.style.top=mousePos.y+2+'px';
+        // console.log(img2.style.left)
+    }
+    function mouseCoords(ev) {
+        if (ev.pageX || ev.pageY) {
+            return {x: ev.pageX, y: ev.pageY};
+        }
+        return {
+            x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+            y: ev.clientY + document.body.scrollTop - document.body.clientTop
+        };
+    }
+    console.log('第三次上传')
 })();
