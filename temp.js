2407971dadb925efa43dd50c718b8f8beb2acfe3
huicudemao <1365560542@qq.com>
2022-12-02 21:26:04 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         hello lzp
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world! lianxi!
+// @author       You
+// @match        *://*
+// @icon         none
+// @grant        none
+// ==/UserScript==
+
+// 匿名函数
+(function() {
+    // 启用严格模式
+    'use strict';
+
+    console.log("第一次写脚本")
+})();
