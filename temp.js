41bbb4e08b084a0002f34e380114b510c22f81c0
huicudemao <1365560542@qq.com>
2022-12-06 00:07:27 +0800

    0.5 version

--- temp.js
+++ temp.js
@@ -1,50 +1,13 @@
 // ==UserScript==
 // @name         hello lzp
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world! lianxi!
 // @author       You
 // @match        *://*
 // @match        *://*/*
-// @match        *://pan.baidu.com/disk/home*
-// @match        *://yun.baidu.com/disk/home*
-// @match        *://pan.baidu.com/disk/main*
-// @match        *://yun.baidu.com/disk/main*
-// @match        *://pan.baidu.com/s*
-// @match        *://yun.baidu.com/s*
-// @match        *://*.youku.com/*
-// @match        *://*.iqiyi.com/*
-// @match        *://*.iq.com/*
-// @match        *://*.le.com/*
-// @match        *://v.qq.com/*
-// @match        *://m.v.qq.com/*
-// @match        *://*.tudou.com/*
-// @match        *://*.mgtv.com/*
-// @match        *://tv.sohu.com/*
-// @match        *://film.sohu.com/*
-// @match        *://*.1905.com/*
-// @match        *://*.bilibili.com/*
-// @match        *://*.pptv.com/*
-// @match        *://item.taobao.com/*
-// @match        *://chaoshi.detail.tmall.com/*
-// @match        *://detail.tmall.com/*
-// @match        *://detail.tmall.hk/*
-// @match        *://item.jd.com/*
-// @match        *://*.yiyaojd.com/*
-// @match        *://*.liangxinyao.com/*
-// @match        *://music.163.com/*
-// @match        *://y.qq.com/*
-// @match        *://*.kugou.com/*
-// @match        *://*.kuwo.cn/*
-// @match        *://*.ximalaya.com/*
-// @match        *://*.zhihu.com/*
-// @match        *://*.douyin.com/*
-// @match        *://*.kuaishou.com/*
-// @match        *://*.ixigua.com/*
-// @match        *://*.youtube.com/*
-// @match        *://zhidao.baidu.com/*
-// @match        *://bbs.csdn.net/*
-// @match        *://blog.csdn.net/*
+// @match        *://*/*/*
+
 // @icon         none
 // @license      AGPL License
 // @grant        none
@@ -54,11 +17,16 @@
 (function() {
     // 启用严格模式
     'use strict';
-    console.log('第三次上传')
+    // console.log('第四次上传')
+    var link1 = document.createElement('link');
+    document.head.appendChild(link1);
+    link1.rel="stylesheet"
+    link1.href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+
     var img2=document.createElement('img');
     document.body.appendChild(img2);
     img2.src="https://pic.baike.soso.com/p/20120724/20120724100154-699505001.jpg";
-    img2.style.cssText="width:75px;height:75px;position: absolute;z-index: 99999999999 !important"
+    img2.style.cssText="width:75px;height:75px;position: absolute;z-index:99999999 !important"
 
     // 鼠标移动事件
     document.onmousemove =function(ev) {
@@ -78,5 +46,38 @@
             y: ev.clientY + document.body.scrollTop - document.body.clientTop
         };
     }
+
+    var div1=document.createElement('div')
+        document.body.appendChild(div1)
+        div1.style="border-radius:50%;opacity:0;width:400px;height:400px;background-color: #f5f5f5;z-index:-1;position: absolute;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;pointer-events:none"
+    //鼠标点击事件
+    document.onclick=function(ev){
+
+        if(div1.classList.contains("animate__animated")){
+            // div1.style.animationPlayState="paused"//暂停动画
+            div1.classList.remove("animate__animated","animate__zoomIn")
+            setTimeout(() => {
+                div1.classList.add("animate__animated","animate__zoomIn")
+            }, 0);
+            
+        }else{
+            div1.classList.add("animate__animated","animate__zoomIn")
+        }
+                
+                // div1.style.opacity="0.1";
+
+                console.log('你点击了鼠标')
+
+                var Ev = ev || window.event;
+                var mousePos = mouseCoords(Ev);
+
+                div1.style.left=mousePos.x-200+'px';
+                div1.style.top=mousePos.y-200+'px';
+                
+                setTimeout(() => {
+                    div1.classList.remove("animate__animated","animate__zoomIn")
+                    // div1.style.opacity="0"
+                }, 1000);
+        }
     
 })();
